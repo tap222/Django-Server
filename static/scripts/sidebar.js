d3d33732ce3c5ebe8f55a7acbf0e020112b8de7f
Achraf Khadraoui <75825038+AchrafTheGeek@users.noreply.github.com>
2026-01-26 05:52:38 +0100

    removed static from the ignore file

---
+++ static/scripts/sidebar.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Toggle Sidebar
+  const sidebar = document.getElementById("sidebar");
+  const openSidebar = document.getElementById("openSidebar");
+  const closeSidebar = document.getElementById("closeSidebar");
+
+  openSidebar.addEventListener("click", () => {
+    sidebar.classList.remove("-translate-x-full");
+  });
+
+  closeSidebar.addEventListener("click", () => {
+    sidebar.classList.add("-translate-x-full");
+  });
+});
